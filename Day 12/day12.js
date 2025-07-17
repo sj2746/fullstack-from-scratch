0f8809889bd65587cbbc3c55c1df055554871f68
sj2746 <bhatsajan2005@gmail.com>
2025-07-17 20:47:12 +0530

    Add files via upload

---
+++ Day 12/day12.js
@@ -0,0 +1,21 @@
+//Function example
+function fun(){
+	console.log("Sum ans here");
+}
+fun(); 			//Declaring
+	
+//creating a function
+let a=10;
+let b=5;
+function sum(){
+	console.log(a+b);
+}
+sum();		//Declating
+
+//Function with arguments
+function add(num1,num2){
+	result=num1+num2;
+	console.log(result);
+}
+add(8,5);
+	
