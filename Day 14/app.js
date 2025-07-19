323853550ac404ca17aa1693ffecfc655322ec7a
sj2746 <bhatsajan2005@gmail.com>
2025-07-19 19:02:56 +0530

    Add files via upload

---
+++ Day 14/app.js
@@ -0,0 +1,27 @@
+// <img src="spiderman_img.png" id="mainImg">- img object
+// let smi=document.getElementsByClassName("oldImg");
+// for(let i=0;i<smi.length;i++)
+// {
+	// console.dir(smi,[i]);
+// }
+
+// console.dir(document.querySelector(".box"));
+
+
+
+//innerText - Prints contents that are visible
+
+//innerHTML - Prints all content+tags used there.
+
+//textConetent - how we written text in our (.html) file.
+
+let llinks=document.querySelectorAll('.box a');
+for (link of llinks){
+	link.style.color="purple";
+}
+
+// for(let i=0;i<llinks.length;i++){
+	// llinks[i].style.color="green";
+// }
+
+
