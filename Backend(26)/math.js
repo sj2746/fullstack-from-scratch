e3db05f8e68abc5b9bf0faf29d5e4301a0c3782c
sajan <bhatsajan2005@gmail.com>
2025-07-31 18:18:03 +0530

    process

---
+++ Backend(26)/math.js
@@ -0,0 +1,50 @@
+// //METHOD 1
+// const sum=(a,b)=> a+b;
+// const mul=(a,b)=> a*b;
+// const g= 9.8; 
+// const PI= 3.14; 
+
+// module.exports={
+	// sum:sum,
+	// mul:mul,
+	// g:g,
+	// PI:PI
+// };
+//--------------------------------------------------------------
+// //METHOD 2
+// const sum=(a,b)=> a+b;
+// const mul=(a,b)=> a*b;
+// const g= 9.8; 
+// const PI= 3.14; 
+//--------------------------------------------------------------
+// let obj={
+	// sum:sum,
+	// mul:mul,
+	// g:g,
+	// PI:PI
+// };
+// module.exports=obj;
+//--------------------------------------------------------------
+// //METHOD 3
+// const sum=(a,b)=> a+b;
+// const mul=(a,b)=> a*b;
+// const g= 9.8; 
+// const PI= 3.14; 
+
+// module.exports={
+	// sum:sum,
+	// mul:mul,
+	// g:g,
+	// PI:PI
+// };
+//--------------------------------------------------------------
+//METHOD 4
+module.exports.sum=(a,b)=> a+b;
+exports.mul=(a,b)=> a*b;	
+exports.g= 9.8; 		//exports=5(Error!Treats as normal Var[without '.'])
+module.exports.PI= 3.14; 
+
+
+
+
+
---
+++ Backend(26)/script.js
@@ -0,0 +1,31 @@
+// //METHOD 1
+// const math=require("./math");
+// console.log(math.sum(2.2,2.3));
+// console.log(math.mul(2.2,2.3));
+// console.log(math.g);
+// console.log(math.PI);
+//--------------------------------------------------------------
+// //Method 2
+// const math=require("./math");
+// console.log(math.sum(2.2,2.3));
+// console.log(math.mul(2.2,2.3));
+// console.log(math.g);
+// console.log(math.PI);
+//--------------------------------------------------------------
+// //Method 3
+// const math=require("./math");
+// console.log(math.sum(2.2,2.3));
+// console.log(math.mul(2.2,2.3));
+// console.log(math.g);
+// console.log(math.PI);
+//--------------------------------------------------------------
+//Method 4
+const math=require("./math");
+
+console.log(math.sum(2.2,2.3));
+console.log(math.mul(2.2,2.3));
+console.log(math.g);
+console.log(math.PI);
+
+
+
